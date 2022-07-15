6ca3399a6b6d430b035762c185cf12ef41ca72b3
Christian M <chriamue@gmail.com>
2022-07-15 15:48:56 +0200

    :sparkles: updated report problem protocol

--- README.md
+++ README.md
@@ -27,5 +27,5 @@ wasm-pack test --node
 | [did exchange](https://github.com/hyperledger/aries-rfcs/blob/main/features/0023-did-exchange)            |             | :large_orange_diamond: |           |                    |                          |
 | [issue credential](https://github.com/hyperledger/aries-rfcs/blob/main/features/0453-issue-credential-v2) |             | :large_orange_diamond: |           |                    |                          |
 | [present proof](https://github.com/hyperledger/aries-rfcs/blob/main/features/0454-present-proof-v2)       |             | :large_orange_diamond: |           |                    |                          |
-| [report problem](https://github.com/hyperledger/aries-rfcs/blob/main/features/0035-report-problem)        |             | :large_orange_diamond: |           |                    |                          |
+| [report problem](https://identity.foundation/didcomm-messaging/spec/#problem-reports)        |             |  | :large_orange_diamond: |                    |                          |
 | [trust ping](https://identity.foundation/didcomm-messaging/spec/#trust-ping-protocol-20)                  |             |                        |           | :heavy_check_mark: | Finished implementation. |
--- src/reportproblem.rs
+++ src/reportproblem.rs
@@ -1,24 +1,30 @@
-//! # Report Problem Protocol 1.0
+//! # Report Problem Protocol 2.0
 //!
 //! Describes how to report errors and warnings in a powerful, interoperable way. All implementations of SSI agent or hub technology SHOULD implement this RFC.
-//! <https://github.com/hyperledger/aries-rfcs/blob/main/features/0035-report-problem/README.md>
+//! <https://identity.foundation/didcomm-messaging/spec/#problem-reports>
 
 use didcomm_rs::Message;
-use serde_json::Value;
+use serde_json::{json, Value};
 
 #[derive(Default)]
 pub struct ReportProblemResponseBuilder {
     message: Option<Message>,
-    description: Option<String>,
-    problem_items: Vec<Value>,
+    code: Option<String>,
+    comment: Option<String>,
+    args: Vec<String>,
+    escalate_to: Option<String>,
+    ack: Vec<String>,
 }
 
 impl ReportProblemResponseBuilder {
     pub fn new() -> Self {
         ReportProblemResponseBuilder {
             message: None,
-            description: None,
-            problem_items: Vec::new(),
+            code: None,
+            comment: None,
+            args: Vec::new(),
+            escalate_to: None,
+            ack: Vec::new(),
         }
     }
 
@@ -27,23 +33,59 @@ impl ReportProblemResponseBuilder {
         self
     }
 
-    pub fn description(&mut self, description: String) -> &mut Self {
-        self.description = Some(description);
+    pub fn code(&mut self, code: String) -> &mut Self {
+        self.code = Some(code);
         self
     }
 
-    pub fn problem_item(&mut self, problem_item: Value) -> &mut Self {
-        self.problem_items.push(problem_item);
+    pub fn comment(&mut self, comment: String) -> &mut Self {
+        self.comment = Some(comment);
+        self
+    }
+
+    pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+        self.args = args;
+        self
+    }
+
+    pub fn escalate_to(&mut self, escalate_to: String) -> &mut Self {
+        self.escalate_to = Some(escalate_to);
+        self
+    }
+
+    pub fn ack(&mut self, ack: Vec<String>) -> &mut Self {
+        self.ack = ack;
         self
     }
 
     pub fn build(&mut self) -> Result<Message, &'static str> {
-        let mut message =
-            Message::new().m_type("https://didcomm.org/report-problem/1.0/problem-report");
+        let mut message = Message::new()
+            .m_type("https://didcomm.org/report-problem/2.0/problem-report")
+            .body(&serde_json::to_string(&self.build_body()).unwrap());
         if let Some(m) = self.message.as_ref() {
             message = message.thid(&m.get_didcomm_header().id)
         }
+        if !self.ack.is_empty() {
+            message = message
+                .add_header_field("ack".to_string(), serde_json::to_string(&self.ack).unwrap())
+        }
         Ok(message)
+    }
+
+    pub fn build_body(&mut self) -> Value {
+        let mut body: Value = json!({
+            "code": self.code.as_ref().unwrap()
+        });
+        if self.comment.is_some() {
+            body["comment"] = json!(self.comment.as_ref().unwrap());
+        }
+        if !self.args.is_empty() {
+            body["args"] = json!(self.args);
+        }
+        if self.escalate_to.is_some() {
+            body["escalate_to"] = json!(self.escalate_to.as_ref().unwrap());
+        }
+        body
     }
 }
 
@@ -57,12 +99,44 @@ mod tests {
         let thid = message.get_didcomm_header().id.to_string();
         let report = ReportProblemResponseBuilder::new()
             .message(message)
+            .code("a".to_string())
             .build()
             .unwrap();
         assert_eq!(
             report.get_didcomm_header().m_type,
-            "https://didcomm.org/report-problem/1.0/problem-report"
+            "https://didcomm.org/report-problem/2.0/problem-report"
         );
         assert_eq!(report.get_didcomm_header().thid.clone().unwrap(), thid);
+    }
+
+    #[test]
+    fn test_report_problem_as_json() {
+        let message = Message::new();
+        let thid = message.get_didcomm_header().id.to_string();
+        let response = ReportProblemResponseBuilder::new()
+            .message(message)
+            .ack(vec![thid.to_string()])
+            .code("c".to_string())
+            .comment("some comment".to_string())
+            .escalate_to("mailto:admin@foo.org".to_string())
+            .build()
+            .unwrap();
+
+        let didcomm_message = response.as_raw_json().unwrap();
+
+        let json_value: Value = serde_json::from_str(&didcomm_message).unwrap();
+        assert_eq!(
+            json_value["ack"].as_str().unwrap(),
+            serde_json::to_string(&json!(vec!(thid))).unwrap()
+        );
+        assert_eq!(json_value["body"]["code"].as_str().unwrap(), "c");
+        assert_eq!(
+            json_value["body"]["comment"].as_str().unwrap(),
+            "some comment"
+        );
+        assert_eq!(
+            json_value["body"]["escalate_to"].as_str().unwrap(),
+            "mailto:admin@foo.org"
+        );
     }
 }
